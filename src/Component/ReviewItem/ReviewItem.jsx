a5742806017155636769f84d0b8e84dffa2662ea
Progga Roy~ <proggaroy2000@gmail.com>
2023-04-10 10:59:57 -0700

    review cart button add

---
+++ src/Component/ReviewItem/ReviewItem.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import './ReviewItem.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {  faTrashAlt } from '@fortawesome/free-solid-svg-icons'
+
+const ReviewItem = ({product}) => {
+    return (
+        <div className='reviewItem'>
+          <img src={product.img} alt="" />
+
+          <div className='review-details'>
+          
+           <p className='name'>{product.name}</p>
+            <p>Price :<span className='color'> ${product.price}</span> </p>
+            <p>Order quantity:<span className='color'> ${product.quantity}</span></p>
+           </div>
+
+           <button> <FontAwesomeIcon icon={faTrashAlt} className='btn'></FontAwesomeIcon></button>
+         
+         
+        </div>
+    );
+};
+
+export default ReviewItem;
